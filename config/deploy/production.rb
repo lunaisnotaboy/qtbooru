916308b6c72e1617f0c13bd7e9918c7a15e97a67
lunaisnotaboy <her@mint.lgbt>
2022-10-17 13:39:45 -0400

    whoops 2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,6 @@
 set :user, "qtbooru"
 set :rails_env, "production"
 set :delayed_job_workers, 8
-append :linked_files, ".env.production", 'config/newrelic.yml'
+append :linked_files, ".env.production"
 
 server 'athena.local', :roles => %w(web app cron), :user => 'danbooru', :primary => true
-
-after "deploy:updated", "newrelic:notice_deployment"
